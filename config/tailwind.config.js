ce69cee697f1cc676311be9b5ff72ef7d445cab3
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-23 02:32:36 -0300

    Add styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['"Proxima Nova"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        customBlue: {
+          DEFAULT: '#3778c2',
+        },
+        customGreen: {
+          DEFAULT: '#5fb523',
+        },
       },
     },
   },
